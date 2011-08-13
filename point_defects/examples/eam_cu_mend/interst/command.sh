5ab1fd480255f239a9c8ee1e2fd1c4a80d8f1229
bohumir <bohumir@9ef6fc8c-e53f-e1b5-556e-f88c4e1e900d>
2011-08-13 04:40:05 +0000

    added example with Cu potential by Mendelev

---
+++ point_defects/examples/eam_cu_mend/interst/command.sh
@@ -0,0 +1,6 @@
+PATH=$PATH:../../..
+PYTHONPATH=$PYTHONPATH:.
+
+interst.py Cu fcc tetr 3.63908232 > log.Cu.fcc.tetr
+interst.py Cu fcc octa 3.63908232 > log.Cu.fcc.octa
+interst.py Cu fcc dumb 3.63908232 > log.Cu.fcc.dumb
---
+++ point_defects/examples/eam_cu_mend/interst/model.py
@@ -0,0 +1,5 @@
+files = [ "Cu1.eam.fs" ]
+
+pair_coeff = [ "* * " + files[0] ]
+parameters = { "pair_style" : "eam/fs", "pair_coeff" : pair_coeff }
+ext=""

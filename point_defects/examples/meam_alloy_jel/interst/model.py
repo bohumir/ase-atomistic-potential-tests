70a96e4defbfdcb8fd4005b59907486b5d68fbf0
bohumir <bohumir@9ef6fc8c-e53f-e1b5-556e-f88c4e1e900d>
2011-09-24 22:19:27 +0000

    added pick_elements method to model to make it self contained

--- point_defects/examples/meam_alloy_jel/interst/model.py
+++ point_defects/examples/meam_alloy_jel/interst/model.py
@@ -1,5 +1,10 @@
 files = [ "meamf", "meam.alsimgcufe" ]
 
-pair_coeff = [ "* * " + files[0] + " AlS SiS MgS CuS FeS " + files[1] ]
-parameters = { "pair_style" : "meam", "pair_coeff" : pair_coeff }
-ext = "S"
+mypc = [ "* * " + files[0] + " AlS SiS MgS CuS FeS " + files[1] ]
+parameters = { "pair_style" : "meam", "pair_coeff" : mypc }
+
+myext = "S"
+    
+def pick_elements(model, elems):
+    for myel in elems:
+        model.parameters["pair_coeff"][0]  += " " + myel + myext

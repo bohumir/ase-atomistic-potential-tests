538f4cdb4f89e0d7061327ecac71c3698353e51e
bohumir <bohumir@9ef6fc8c-e53f-e1b5-556e-f88c4e1e900d>
2011-09-24 18:04:16 +0000

    improved Al potential

--- point_defects/examples/meam_alloy_jel/monovac/command.sh
+++ point_defects/examples/meam_alloy_jel/monovac/command.sh
@@ -1,8 +1,8 @@
 PATH=$PATH:../../..
 PYTHONPATH=$PYTHONPATH:.
 
-mono.py Al fcc 4.05  > log.Al.fcc
-mono.py Si dia 5.431 > log.Si.dia
-mono.py Cu fcc 3.62  > log.Cu.fcc
-mono.py Fe bcc 2.851 > log.Fe.bcc
-mono.py Mg hcp 3.2027793 0.991824332358 > log.Mg.hcp
+monovac.py Al fcc 4.05  > log.Al.fcc
+monovac.py Si dia 5.431 > log.Si.dia
+monovac.py Cu fcc 3.62  > log.Cu.fcc
+monovac.py Fe bcc 2.851 > log.Fe.bcc
+monovac.py Mg hcp 3.2027793 0.991824332358 > log.Mg.hcp
